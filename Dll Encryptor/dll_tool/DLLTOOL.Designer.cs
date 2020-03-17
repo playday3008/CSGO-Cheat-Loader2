56f4347c47c82ad0b26ccdcb6941ecd98a88de1c
playday3008 <titanv3585@gmail.com>
2020-03-17 03:59:07 +0100

    Add icon Enable some debug functions

--- Dll Encryptor/dll_tool/DLLTOOL.Designer.cs
+++ Dll Encryptor/dll_tool/DLLTOOL.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
 			this.textBox1 = new System.Windows.Forms.TextBox();
 			this.button1 = new System.Windows.Forms.Button();
 			this.label1 = new System.Windows.Forms.Label();
@@ -71,6 +72,7 @@
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.button1);
 			this.Controls.Add(this.textBox1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.Name = "Form1";
 			this.Text = "DLL Encryptor";
